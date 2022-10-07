783383b883041d88eb5b354112237e4f03de7dfc
Emir  Karabeg <emirkarabeg@Emirs-MacBook-Pro.local>
2022-10-06 18:15:04 -0700

    Created week1

--- client/src/components/nav-bar.tsx
+++ client/src/components/nav-bar.tsx
@@ -1,14 +1,16 @@
-import { Box, Flex, Heading, Button, Spacer, Image } from "@chakra-ui/react";
+import { Box, Flex, Heading, Button, Spacer, Image } from '@chakra-ui/react';
 
 const NavBar = () => {
   return (
-    <Flex mx="50px" justify="center">
-      <Flex justify="center">
-        <Image src="logo.svg" alt="Notiom logo" />
-        <Heading>Notiom</Heading>
+    <Flex mx={50} pt={4} align="center">
+      <Flex align="center">
+        <Image src="logo.svg" alt="Notiom logo" w={10} mr={4} />
+        <Heading size="lg" color="#545454" fontWeight="extrabold">
+          Notiom
+        </Heading>
       </Flex>
       <Spacer />
-      <Button>Create</Button>
+      <Button variant="create">Create</Button>
     </Flex>
   );
 };
