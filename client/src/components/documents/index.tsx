2c9627eb7dae2dbc3dd149ede231c12c4737eea9
Emir  Karabeg <emirkarabeg@Emirs-MacBook-Pro.local>
2022-10-06 18:27:33 -0700

    Week 1 complete

--- client/src/components/documents/index.tsx
+++ client/src/components/documents/index.tsx
@@ -13,7 +13,7 @@ import Document from '../documents/document';
 
 const Documents = () => {
   return (
-    <Grid templateColumns="repeat(6, 1fr)" mx={50} py={10}>
+    <Grid templateColumns="repeat(6, 1fr)" mx={50} my={10} gap={8}>
       <GridItem>
         <Link href="/">
           <Image src="/create-document.svg" alt="Create document" w={130} />
